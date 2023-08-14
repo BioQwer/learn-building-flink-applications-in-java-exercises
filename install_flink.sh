d13d4f0ce07e8c497745711b281fedfbddb53b03
Anton Aleksandrov <afaleksandro@detmir.ru>
2023-08-14 10:23:20 +0300

    add kafka

--- install_flink.sh
+++ install_flink.sh
@@ -1,2 +1,5 @@
 curl https://archive.apache.org/dist/flink/flink-1.17.1/flink-1.17.1-bin-scala_2.12.tgz --output flink-1.17.1.tgz
 tar -xzf flink-*.tgz
+
+wget https://dlcdn.apache.org/kafka/3.5.0/kafka_2.13-3.5.0.tgz
+tar -xzf kafka_*.tgz
